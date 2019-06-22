2ce84326dec759e7775a16f9cf303229909b86e8
zly636600 <524136269@qq.com>
2019-06-22 16:46:30 +0800

    update

--- src/DataProvider.js
+++ src/DataProvider.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 export default class DataProvider{
     static getpigjiangCsv(){
-        return axios.get('./data/pigjiang.csv')
+        return axios.get('https://raw.githubusercontent.com/zly636600/graduate/master/src/data/pigjiang.csv')
     }
 
     static getMapJson() {
 
         //console.log(data)
 
-        return axios.get('./data/china.json')
+        return axios.get('https://raw.githubusercontent.com/zly636600/graduate/master/src/data/china.json')
     }
 }
